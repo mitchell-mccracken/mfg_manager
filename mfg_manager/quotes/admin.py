9f910707393806438e821727ab4a3324196c78bc
mitchell-mccracken <mpm5069@gmail.com>
2021-07-07 15:51:33 -0400

    committing before trying another login method

--- mfg_manager/quotes/admin.py
+++ mfg_manager/quotes/admin.py
@@ -4,4 +4,4 @@ from .models import Quote, AppUser
 # Register your models here.
 
 admin.site.register(Quote)
-admin.site.register(AppUser)
+# admin.site.register(AppUser)
--- mfg_manager/quotes/viewsets.py
+++ mfg_manager/quotes/viewsets.py
@@ -43,6 +43,9 @@ class LoginAPI(generics.GenericAPIView):
         serializer = self.get_serializer(data = request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data
+        print(user)
+        # print(request.META)
+        # request.session['username'] = user
         return Response({
             "user": UserSerializer(user, context = self.get_serializer_context()).data,
             "token": AuthToken.objects.create(user)[1]
