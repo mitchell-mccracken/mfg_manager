06bc27316deb32ec833f5b5384ae64869ce5a9f7
mitchell-mccracken <mpm5069@gmail.com>
2021-07-09 15:16:24 -0400

    allowed all access through CORS for testing purposes

--- mfg_manager/settings.py
+++ mfg_manager/settings.py
@@ -61,7 +61,8 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+# CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:8000',
   'http://localhost:3000',
